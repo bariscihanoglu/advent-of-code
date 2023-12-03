4cdfa4537402612b9f336d78f59ea8f648b752c9
BarisCihanoglu <bariscihanoglu4@gmail.com>
2023-12-03 03:28:17 +0300

    Solved part two.

--- day2/src/day2/CubeGameMain.java
+++ day2/src/day2/CubeGameMain.java
@@ -4,7 +4,7 @@ public class CubeGameMain {
 
 	public static void main(String[] args) {
 		//CubeGamePartOne.PartOne();
-		
+		CubeGamePartTwo.PartTwo();
 	}
 
 }
--- day2/src/day2/CubeGamePartTwo.java
+++ day2/src/day2/CubeGamePartTwo.java
@@ -16,6 +16,7 @@ public class CubeGamePartTwo {
 			
 			while(inputReader.hasNextLine())
 			{
+				int r = 1, g = 1, b = 1;
 				ArrayList<String> revealsList = new ArrayList<String>();
 				line = inputReader.nextLine();
 				
@@ -25,11 +26,24 @@ public class CubeGamePartTwo {
 				for(String reveal : reveals)
 				{
 					revealsList.add(reveal);
+					//System.out.println("RevealsList '" + revealsList + "'");
 				}
 				
-				
-				// Integer.parseInt([STRING].replaceAll("[\\D]", ""));
-				
+				for(int i = 1; i < reveals.length; i++) {
+					String[] reveal = reveals[i].split(",");
+					for(int j = 0; j < reveal.length; j++) {
+						if(reveal[j].contains("red") && Integer.parseInt(reveal[j].replaceAll("[\\D]", "")) > r) {
+							r = Integer.parseInt(reveal[j].replaceAll("[\\D]", ""));
+						}
+						else if(reveal[j].contains("green") && Integer.parseInt(reveal[j].replaceAll("[\\D]", "")) > g) {
+							g = Integer.parseInt(reveal[j].replaceAll("[\\D]", ""));
+						}
+						else if(reveal[j].contains("blue") && Integer.parseInt(reveal[j].replaceAll("[\\D]", "")) > b) {
+							b = Integer.parseInt(reveal[j].replaceAll("[\\D]", ""));
+						}
+					}
+				}
+				result += r * g * b;
 				
 				System.out.println("Total: " + result);
 			}
