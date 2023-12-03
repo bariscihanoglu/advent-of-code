b616ecc95b153ca558d36c2a30067836f8f40da6
BarisCihanoglu <bariscihanoglu4@gmail.com>
2023-12-03 03:36:54 +0300

    Cleaner code. Deleted unnecessary codes.

--- day2/src/day2/CubeGamePartTwo.java
+++ day2/src/day2/CubeGamePartTwo.java
@@ -12,41 +12,34 @@ public class CubeGamePartTwo {
 		try {
 			Scanner inputReader = new Scanner(input);
 			int result = 0;
-			String line;
 			
 			while(inputReader.hasNextLine())
 			{
 				int r = 1, g = 1, b = 1;
-				ArrayList<String> revealsList = new ArrayList<String>();
-				line = inputReader.nextLine();
-				
-				System.out.println(line);
+				String line = inputReader.nextLine();
 				
 				String[] reveals = line.split(":|;");
-				for(String reveal : reveals)
-				{
-					revealsList.add(reveal);
-					//System.out.println("RevealsList '" + revealsList + "'");
-				}
 				
 				for(int i = 1; i < reveals.length; i++) {
+					
 					String[] reveal = reveals[i].split(",");
+					
 					for(int j = 0; j < reveal.length; j++) {
-						if(reveal[j].contains("red") && Integer.parseInt(reveal[j].replaceAll("[\\D]", "")) > r) {
-							r = Integer.parseInt(reveal[j].replaceAll("[\\D]", ""));
+						int count = Integer.parseInt(reveal[j].replaceAll("[\\D]", ""));
+						if(reveal[j].contains("red") && count > r) {
+							r = count;
 						}
-						else if(reveal[j].contains("green") && Integer.parseInt(reveal[j].replaceAll("[\\D]", "")) > g) {
-							g = Integer.parseInt(reveal[j].replaceAll("[\\D]", ""));
+						else if(reveal[j].contains("green") && count > g) {
+							g = count;
 						}
-						else if(reveal[j].contains("blue") && Integer.parseInt(reveal[j].replaceAll("[\\D]", "")) > b) {
-							b = Integer.parseInt(reveal[j].replaceAll("[\\D]", ""));
+						else if(count > b) {
+							b = count;
 						}
 					}
 				}
 				result += r * g * b;
-				
-				System.out.println("Total: " + result);
 			}
+			System.out.println("Total: " + result);
 			inputReader.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found.");
